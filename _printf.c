c5d905dc85c225f81cb80c3459ad48a03cab18d4
jacobmohomutsialx <jacobmohomutsi+alx@gmail.com>
2023-03-28 17:53:41 -0700

    Bug Fix

--- _printf.c
+++ _printf.c
@@ -1,27 +1,27 @@
-#include <stddef.h>
+i#include <stddef.h>
 #include <stdio.h>
 
 
 /**
  * _printf - function that prints output
- * @format: is a character string
- * Return: the number of characters to be printed
+ * @format: is a charachter string with 0 to 3 directives
+ * Return: the number of charachters to be printed
  */
 
 int _printf(const char *format, ...)
 {
 	va_list valist;
 	int i, buffend = 0;
-	double tBuffer = 0;
-	double *t;
-	char *h;
+	double totalBuffer = 0;
+	double *total;
+	char *holder;
 	char buffer[BUFSIZE];
 	char *(*spec_func)(va_list) = NULL;
 
 	if (!format)
 		return (-1);
 	va_start(valist, format);
-	t = &tBuffer;
+	total = &totalBuffer;
 	for (i = 0; i < BUFSIZE; i++)
 		buffer[i] = 0;
 	for (i = 0; format && format[i]; i++)
@@ -30,17 +30,17 @@ int _printf(const char *format, ...)
 		{
 			i++;
 			spec_func = get_spec_func(format[i]);
-			h = (spec_func) ? spec_func(valist) : nothing_found(format[i]);
-			if (h)
-				buffend = alloc_buffer(h, _strlen(h), buffer, buffend, t);
+			holder = (spec_func) ? spec_func(valist) : nothing_found(format[i]);
+			if (holder)
+				buffend = alloc_buffer(holder, _strlen(holder), buffer, buffend, total);
 		}
 		else
 		{
-			h = chartos(format[i]);
-			buffend = alloc_buffer(h, 1, buffer, buffend, t);
+			holder = chartos(format[i]);
+			buffend = alloc_buffer(holder, 1, buffer, buffend, total);
 		}
 	}
 	_puts(buffer, buffend);
 	va_end(valist);
-	return (tBuffer + buffend);
+	return (totalBuffer + buffend);
 }
--- main.h
+++ main.h
@@ -1,5 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+char *_memcpy(char *dest, char *src, unsigned int n, unsigned int bufferlen);
+int alloc_buffer(char *hold, int hlen, char *buffer, int size, double *total);
+char *(*get_spec_func(char s))(va_list);
+char *rot13_find(va_list s);
+char *rev_find(va_list s);
+char *binary_find(va_list n);
+char *octal_find(va_list n);
 int _printf(const char *format, ...);
 #endif
